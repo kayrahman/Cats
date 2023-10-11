5b39fcb6fbe4c062fea185220a733eb0982728ff
Md. Khalilur Rahman <khalilur.rahman@bjitgroup.com>
2023-10-11 17:27:07 +0600

    Implemented a detail screen and passing data between the screen.

--- lib/apimodule/api_service.dart
+++ lib/apimodule/api_service.dart
@@ -1,67 +1,133 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<List<CatImage>> fetchPhotos({
+Future<List<Cat>> fetchPhotos({
   int page = 1,
   int perPage = 10,
   String orderBy = 'latest',
 }) async {
   //final String apiUrl = 'https://api.unsplash.com/photos';
-  final String apiUrl = 'https://api.thecatapi.com/v1/images/search?format=json&limit=10';
+  final String apiUrl =
+      'https://api.thecatapi.com/v1/images/search?format=json&limit=10';
   final response = await http.get(
     Uri.parse(apiUrl),
     headers: {
       'Content-Type': 'application/json',
-      'x-api-key': 'YOUR_OWN_API_KEY',
+      'x-api-key':
+          'live_YD6cYybx8FKastDhWRwsmjNaTRdQYRB78yRCBXHEOcOrNUSLMOZ2l1Ifh53JzPKO',
     },
   );
 
   if (response.statusCode == 200) {
     final List<dynamic> jsonData = json.decode(response.body);
-    final List<CatImage> catImages = jsonData.map((data) {
-      return CatImage.fromJson(data);
+    final List<Cat> catImages = jsonData.map((data) {
+      return Cat.fromJson(data);
     }).toList();
     print('cat_images > ${catImages[0].url}');
     return catImages;
   } else {
-    print('API Error: Failed to load cat images. Status code: ${response
-        .statusCode}');
+    print(
+        'API Error: Failed to load cat images. Status code: ${response.statusCode}');
     throw Exception(
         'Failed to load cat images. Status code: ${response.statusCode}');
   }
 }
 
+Future<CatInfo> fetchCatDetail() async {
+  //final String apiUrl = 'https://api.unsplash.com/photos';
+  final String apiUrl = 'https://api.thecatapi.com/v1/images/0XYvRd7oD';
+  final response = await http.get(
+    Uri.parse(apiUrl),
+   /* headers: {
+      'Content-Type': 'application/json',
+      'x-api-key':
+          'live_YD6cYybx8FKastDhWRwsmjNaTRdQYRB78yRCBXHEOcOrNUSLMOZ2l1Ifh53JzPKO',
+    },*/
+  );
 
-
-
-class UnsplashPhoto {
-final String id;
-final String description;
-final String imageUrl;
-final String author;
-
-UnsplashPhoto({
-required this.id,
-required this.description,
-required this.imageUrl,
-required this.author,
-});
+  if (response.statusCode == 200) {
+    //final List<dynamic> jsonData = json.decode(response.body);
+    final dynamic jsonData = json.decode(response.body);
+    final CatInfo catInfo = CatInfo.fromJson(jsonData);
+    print('cat_breed_info > ${catInfo.url}');
+    return catInfo;
+  } else {
+    print(
+        'API Error: Failed to load cat images. Status code: ${response.statusCode}');
+    throw Exception(
+        'Failed to load cat images. Status code: ${response.statusCode}');
+  }
 }
 
+class Cat {
+  final String id;
+  final String url;
 
-class CatImage {
-final String id;
-final String url;
+  Cat({
+    required this.id,
+    required this.url,
+  });
 
-CatImage({
-required this.id,
-required this.url,
-});
-
-factory CatImage.fromJson(Map<String, dynamic> json) {
-return CatImage(
-id: json['id'],
-url: json['url'],
-);
+  factory Cat.fromJson(Map<String, dynamic> json) {
+    return Cat(
+      id: json['id'],
+      url: json['url'],
+    );
+  }
 }
+
+class CatInfo {
+  final String url;
+  final List<CatBreedInfo> breeds;
+
+  CatInfo({
+    required this.url,
+    required this.breeds,
+  });
+
+  factory CatInfo.fromJson(Map<String, dynamic> json) {
+    final List<dynamic> breedData = json['breeds'];
+    final List<CatBreedInfo> breedInfo =
+        breedData.map((breedJson) => CatBreedInfo.fromJson(breedJson)).toList();
+
+    return CatInfo(
+      url: json['url'],
+      breeds: breedInfo,
+    );
+  }
+}
+
+class CatBreedInfo {
+  final String temperament;
+  final String origin;
+  final String lifeSpan;
+  final String wikipediaUrl;
+  final String weightImperial;
+  final String weightMetric;
+  final String countryCodes;
+  final String countryCode;
+
+  CatBreedInfo({
+    required this.temperament,
+    required this.origin,
+    required this.lifeSpan,
+    required this.wikipediaUrl,
+    required this.weightImperial,
+    required this.weightMetric,
+    required this.countryCodes,
+    required this.countryCode,
+  });
+
+  factory CatBreedInfo.fromJson(Map<String, dynamic> json) {
+    return CatBreedInfo(
+      temperament: json['temperament'],
+      origin: json['origin'],
+      lifeSpan: json['life_span'],
+      wikipediaUrl: json['wikipedia_url'],
+      weightImperial: json['weight']['imperial'],
+      weightMetric: json['weight']['metric'],
+      countryCodes: json['country_codes'],
+      countryCode: json['country_code'],
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:learning_dart/apimodule/api_service.dart';
 
 void main() {
@@ -11,9 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Your App Title',
+      title: 'BJIT Academy',
       theme: ThemeData(
-        // Your app's theme configuration
+        colorSchemeSeed: const Color(0xff6750a4),
+        useMaterial3: true,
       ),
       home: MyHomePage(),
     );
@@ -26,19 +28,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<CatImage> catImages = [];
+  List<Cat> cats = [];
+  bool shadowColor = true;
 
   @override
   void initState() {
     super.initState();
-   // _loadCatImages();
+    // _loadCatImages();
   }
 
   Future<void> _loadCatImages() async {
     try {
-      final List<CatImage> fetchedImages = await fetchPhotos();
+      final List<Cat> fetchedImages = await fetchPhotos();
       setState(() {
-        catImages = fetchedImages;
+        cats = fetchedImages;
       });
     } catch (e) {
       // Handle error
@@ -47,11 +50,27 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style = TextButton.styleFrom(
+      foregroundColor: Theme.of(context).colorScheme.onPrimary,
+    );
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Unsplash Image Viewer', textDirection: TextDirection.ltr),
-      ),
+          title: Text('Cats', textDirection: TextDirection.ltr),
+          shadowColor:
+              shadowColor ? Theme.of(context).colorScheme.shadow : null,
+          actions: <Widget>[
+            TextButton(
+              style: style,
+              onPressed: () {},
+              child: const Text('Action 1'),
+            ),
+            TextButton(
+              style: style,
+              onPressed: () {},
+              child: const Text('Action 2'),
+            ),
+          ]),
       body: Column(
         children: [
           ElevatedButton(
@@ -59,41 +78,48 @@ class _MyHomePageState extends State<MyHomePage> {
               // Call the fetchPhotos function when the button is pressed
               _loadCatImages();
             },
-            child:  Text('Load Photos', textDirection: TextDirection.ltr),
+            child: Text('Load Photos', textDirection: TextDirection.ltr),
           ),
           Expanded(
             child: GridView.builder(
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                childAspectRatio: 1
-
-              ),
-              itemCount: catImages.length,
+                  crossAxisCount: 3, childAspectRatio: 1),
+              itemCount: cats.length,
               itemBuilder: (context, index) {
-                final photo = catImages[index];
+                final cat = cats[index];
                 return GestureDetector(
                   onTap: () {
                     // Handle tap on the photo if needed
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CatDetailScreen(cat)),
+                    );
                   },
                   child: Card(
                     child: Column(
                       children: [
                         //Image.network('https://googleflutter.com/sample_image.jpg'),
                         CachedNetworkImage(
-                          imageUrl: photo.url,
-                          height: 200, // Set the desired height
-                          width: 200,  // Set the desired width
-                          fit: BoxFit.cover, // Adjust the fit as needed
-                          placeholder: (context, url) => CircularProgressIndicator(), // Placeholder widget
+                          imageUrl: cat.url,
+                          height: 200,
+                          // Set the desired height
+                          width: 200,
+                          // Set the desired width
+                          fit: BoxFit.cover,
+                          // Adjust the fit as needed
+                          placeholder: (context, url) =>
+                              CircularProgressIndicator(),
+                          // Placeholder widget
                           errorWidget: (context, url, error) {
-                            print("Error loading image: $error"); // Print the error message
+                            print(
+                                "Error loading image: $error"); // Print the error message
                             return Icon(Icons.error); // Error widget
                           }, // Error widget
                         ),
-                        Text(photo.id, textDirection: TextDirection.ltr),
+                        Text(cat.id, textDirection: TextDirection.ltr),
                       ],
                     ),
-
                   ),
                 );
               },
@@ -103,8 +129,79 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-
 }
 
+class CatDetailScreen extends StatefulWidget {
+  final Cat cat;
 
+  CatDetailScreen(this.cat);
 
+  @override
+  _CatDetailScreenState createState() => _CatDetailScreenState();
+}
+
+class _CatDetailScreenState extends State<CatDetailScreen> {
+  CatInfo? catInfo;
+  bool loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    loading = true;
+    _loadCatImages();
+  }
+
+  Future<void> _loadCatImages() async {
+    try {
+      final CatInfo info = await fetchCatDetail();
+      setState(() {
+        catInfo = info;
+        loading = false;
+      });
+    } catch (e) {
+      // Handle error
+      print('cat_breed_info_error > $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cat Details'),
+      ),
+      body: Center(
+        child: loading
+            ? CircularProgressIndicator() // Show a loading indicator while loading
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Image.network(catInfo?.url ?? "")
+                  ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Cat Temperament: ${catInfo?.breeds[0].temperament}',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text(
+                      'Cat Origin: ${catInfo?.breeds[0].origin}',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text(
+                      'Cat Life Span: ${catInfo?.breeds[0].lifeSpan}',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // Perform network calls and state updates here if needed
+                      },
+                      child: Text('Learn More'),
+                    ),
+
+                ],
+              ),
+      ),
+    );
+  }
+}
